8cab4d6713ca52d9f75ee9e3e5351441170fcb58
alevento <a@a.it>
2016-09-07 19:49:08 +0200

    Substitute

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,7 @@
 
 All Notable changes to `:package_name` will be documented in this file
 
-## alpha - @@@date
+## alpha - 07.09.16
 
 ### Added
 - Initial commit
--- tests/config/sedCommandProvider.sh
+++ tests/config/sedCommandProvider.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed -i "s/'providers' => \[/'providers' => \[ @@@namespacevendor\\\\@@@namespacepackage_name\\\\@@@providerpackage_nameServiceProvider::class,/g" ./config/app.php
+sed -i "s/'providers' => \[/'providers' => \[ Padosoft\\\\Www\\\\WwwServiceProvider::class,/g" ./config/app.php
